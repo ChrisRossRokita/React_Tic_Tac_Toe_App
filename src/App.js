73b627f65c9495e34a0b0e7b5fe115eec00d0bf7
ChrisRossRokita <88979453+ChrisRossRokita@users.noreply.github.com>
2021-08-21 05:12:30 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Tic Tac Toe
     </div>
   );
 }
+
+
+class Square extends React.Component {
+  render() {
+    return (
+    <button className="square">
+      {this.props.value}
+    </button> 
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSqaure(i) {
+    return <Square value={i} />;
+  };
+
+  render() {
+    const status = 'Next player: X';
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+        {this.renderSquare(0)}
+        {this.renderSquare(1)}
+        {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSqaure(3)}
+          {this.renderSqaure(4)}
+          {this.renderSqaure(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSqaure(6)}
+          {this.renderSqaure(7)}
+          {this.renderSqaure(8)}
+        </div>
+      </div>
+    );
+  }
+}
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{ /* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
 
 export default App;
